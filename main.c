f55552ce70a1bfb10301e6eba88e1cdafdabf921
leticiapcampos <cl562274@gmail.com>
2024-04-05 02:25:29 -0300

    teste de commit 2

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 int main(void){
+
+  int n1, n2, mult;
+
+  printf("digite o primeiro numero: ");
+  scanf("%d",&n1);
+  printf("digite o segundo numero: ");
+  scanf("%d",&n2);
+  mult = n1 * n2;
+  printf("a multiplicação entre os dois numero inseridos e: %d",  mult);
+
+  return 0;
   
-  printf("ola, mundo!!");
 }
